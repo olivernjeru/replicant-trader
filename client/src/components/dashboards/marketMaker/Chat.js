221f5fbe552ccd461937e007000eec9e39c98b1f
Oliver Njeru <olivernjiru@gmail.com>
2024-03-03 21:26:47 +0300

    add chat ui skeleton

--- client/src/components/dashboards/marketMaker/Chat.js
+++ client/src/components/dashboards/marketMaker/Chat.js
@@ -1,12 +1,35 @@
 import * as React from "react";
 import { Box, TextField, Button, Typography, Grid, Paper } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Divider from "@mui/material/Divider";
+import './Chat.css'
 
 const messages = [
   { id: 1, text: "Hi there!", sender: "client" },
   { id: 2, text: "Hello!", sender: "user" },
   { id: 3, text: "How can I assist you today?", sender: "client" },
 ];
+
+const defaultTheme = createTheme({
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiInputLabel-root, & .MuiOutlinedInput-input': {
+            color: 'white', // Change text color to white
+          },
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'white', // Change border color to white
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#1F63E8', // Change border color on hover to white
+          },
+        },
+      },
+    },
+  },
+});
 
 export default function Chat() {
   const [input, setInput] = React.useState("");
@@ -23,22 +46,24 @@ export default function Chat() {
   };
 
   return (
+    <ThemeProvider theme={defaultTheme}>
     <Box
       sx={{
         height: "40vh",
         display: "flex",
         flexDirection: "column",
-        bgcolor: "grey.200",
+        // bgcolor: "grey.200",
       }}
     >
-      <Box sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
+      <Box sx={{ flexGrow: 1, p: 2 }}>
         {messages.map((message) => (
           <Message key={message.id} message={message} />
         ))}
       </Box>
-      <Box sx={{ p: 3, backgroundColor: "background.default" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={10}>
+      <Divider />
+      <Box sx={{ p: 3, backgroundColor: "background.primary" }}>
+        <Grid container spacing={1}>
+          <Grid item xs={0}>
             <TextField
               size="small"
               fullWidth
@@ -48,7 +73,7 @@ export default function Chat() {
               onChange={handleInputChange}
             />
           </Grid>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <Button
               fullWidth
               color="primary"
@@ -62,6 +87,7 @@ export default function Chat() {
         </Grid>
       </Box>
     </Box>
+    </ThemeProvider>
   );
 };
 
