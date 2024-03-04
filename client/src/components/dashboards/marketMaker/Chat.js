a5e44e76f9c0583d326489916da0e14a94182554
Oliver Njeru <olivernjiru@gmail.com>
2024-03-04 23:16:54 +0300

    discovered floating action button mui

--- client/src/components/dashboards/marketMaker/Chat.js
+++ client/src/components/dashboards/marketMaker/Chat.js
@@ -1,8 +1,11 @@
 import * as React from "react";
-import { Box, TextField, Button, Typography, Grid, Paper } from "@mui/material";
+import { Box, TextField, Button, Typography, Grid, Paper, InputAdornment, IconButton } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Divider from "@mui/material/Divider";
+import Container from "@mui/material/Container";
+import SearchIcon from '@mui/icons-material/Search';
+import { Avatar } from "@mui/material";
 import './Chat.css'
 
 const messages = [
@@ -45,49 +48,84 @@ export default function Chat() {
     setInput(event.target.value);
   };
 
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-    <Box
-      sx={{
-        height: "40vh",
-        display: "flex",
-        flexDirection: "column",
-        // bgcolor: "grey.200",
-      }}
-    >
-      <Box sx={{ flexGrow: 1, p: 2 }}>
-        {messages.map((message) => (
-          <Message key={message.id} message={message} />
-        ))}
-      </Box>
-      <Divider />
-      <Box sx={{ p: 3, backgroundColor: "background.primary" }}>
-        <Grid container spacing={1}>
-          <Grid item xs={0}>
-            <TextField
-              size="small"
-              fullWidth
-              placeholder="Type a message"
-              variant="outlined"
-              value={input}
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <Button
-              fullWidth
-              color="primary"
-              variant="contained"
-              endIcon={<SendIcon />}
-              onClick={handleSend}
-            >
-              Send
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
-    </ThemeProvider>
+    <Container sx={{ bgcolor: "yellow", display: 'flex' }}>
+      <ThemeProvider theme={defaultTheme}>
+        <Box>
+          <TextField
+            label="Search"
+            variant="outlined"
+            size="small"
+            fullWidth
+            sx={{ mb: 3 }}
+            value={searchTerm}
+            onChange={handleChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton aria-label="search">
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Divider />
+        </Box>
+        <Box
+          sx={{
+            height: "40vh",
+            display: "flex",
+            flexDirection: "column",
+            padding: '1%'
+            // bgcolor: "grey.200",
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Avatar></Avatar>
+            <Typography variant="body1" gutterBottom sx={{ paddingLeft: '12%', paddingRight: '12%' }}>JANE DOE</Typography>
+            <Typography variant="body1" gutterBottom>543 789 8890</Typography>
+          </Box>
+          <Box sx={{ flexGrow: 1, p: 2 }}>
+            {messages.map((message) => (
+              <Message key={message.id} message={message} />
+            ))}
+          </Box>
+          <Divider />
+          <Box sx={{ p: 3, backgroundColor: "background.primary" }}>
+            <Grid container spacing={1}>
+              <Grid item xs={0}>
+                <TextField
+                  size="small"
+                  fullWidth
+                  placeholder="Type a message"
+                  variant="outlined"
+                  value={input}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  fullWidth
+                  color="primary"
+                  variant="contained"
+                  endIcon={<SendIcon />}
+                  onClick={handleSend}
+                >
+                  Send
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </Container>
   );
 };
 
