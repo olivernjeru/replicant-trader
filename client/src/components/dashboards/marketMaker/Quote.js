0198ed500e39518b8a520d6839f9c503c45dfa63
Oliver Njeru <olivernjiru@gmail.com>
2024-03-04 13:18:04 +0300

    add quote ui shell

--- client/src/components/dashboards/marketMaker/Quote.js
+++ client/src/components/dashboards/marketMaker/Quote.js
@@ -1,5 +1,115 @@
 import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+const defaultTheme = createTheme({
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    '& .MuiInputLabel-root, & .MuiOutlinedInput-input': {
+                        color: 'white', // Change text color to white
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'white', // Change border color to white
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                        borderColor: '#1F63E8', // Change border color on hover to white
+                    },
+                },
+            },
+        },
+    },
+});
 
 export default function Quote() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          username: data.get('username'),
+          password: data.get('password'),
+        });
+      };
 
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="sm">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        padding: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <Box sx={{display: 'flex'}}>
+                    <TextField
+                            margin="normal"
+                            required
+                            id="stock-ticker-input"
+                            label="TSLA"
+                            name="stock-ticker-input"
+                            autoComplete="stock-ticker-input"
+                            sx={{margin: 1}}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            id="bid"
+                            label="Bid"
+                            name="bid"
+                            autoComplete="bid"
+                            sx={{margin: 1}}
+                        />
+                    </Box>
+                    <Box sx={{display: 'flex'}}>
+                        <TextField
+                            margin="normal"
+                            required
+                            id="offer"
+                            label="Offer"
+                            name="offer"
+                            autoComplete="offer"
+                            sx={{margin: 1}}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            id="volume"
+                            label="Vol"
+                            name="volime"
+                            autoComplete="volume"
+                            sx={{margin: 1}}
+                        />
+                        </Box>
+                        <TextField
+                            margin="normal"
+                            required
+                            id="valid-for"
+                            label="Valid for"
+                            name="valid-for"
+                            autoComplete="valid-for"
+                            sx={{margin: 1}}
+                        />
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            SEND QUOTE
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 }
