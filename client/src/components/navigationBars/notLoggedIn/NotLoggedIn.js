cf2f8dfbd5cd12b252971c2b1003036a0276570c
Oliver Njeru <olivernjiru@gmail.com>
2024-03-15 22:01:26 +0300

    format code

--- client/src/components/navigationBars/notLoggedIn/NotLoggedIn.js
+++ client/src/components/navigationBars/notLoggedIn/NotLoggedIn.js
@@ -5,24 +5,25 @@ import Toolbar from '@mui/material/Toolbar';
 import { Link } from '@mui/material';
 
 export default function NotLoggedIn() {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <div className="left">
-            <Link href="/">Trade</Link>
-          </div>
-          <div className="center">
-            <Link href="">About</Link>
-            <Link href="">What we do</Link>
-            <Link href="">Foundation</Link>
-            <Link href="">Capital</Link></div>
-          <div className="right">
-            <Link href="/login">Login</Link>
-          </div>
-        </Toolbar>
-        <div id="body">
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <div className="left">
+          <Link href="/">Trade</Link>
+        </div>
+        <div className="center">
+          <Link href="">About</Link>
+          <Link href="">What we do</Link>
+          <Link href="">Foundation</Link>
+          <Link href="">Capital</Link>
+        </div>
+        <div className="right">
+          <Link href="/login">Login</Link>
+        </div>
+      </Toolbar>
+      <div id="body">
 
       </div>
-      </AppBar>
-    );
-  }
+    </AppBar>
+  );
+}
