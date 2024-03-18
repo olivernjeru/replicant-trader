5602158ffa633015575a5f5a1f5686e39b8f6599
Oliver Njeru <olivernjiru@gmail.com>
2024-03-18 13:48:22 +0300

    hide firebase keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,17 +7,18 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDzCqh8W0klj90kAW7JRL4mnoDHAYDJKJk",
-  authDomain: "trade-96ab9.firebaseapp.com",
-  projectId: "trade-96ab9",
-  storageBucket: "trade-96ab9.appspot.com",
-  messagingSenderId: "976415999393",
-  appId: "1:976415999393:web:32386ae5126faf3bff37e0",
-  measurementId: "G-S7JYC4PHW1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
