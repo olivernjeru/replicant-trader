8775dfc73124cd4310f0390f0f68e0a00c05879e
Oliver Njeru <olivernjiru@gmail.com>
2024-04-20 12:02:11 +0300

    finally created PrivateRoutes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import LogInPage from './pages/LogInPage';
 import ResetPasswordPage from './pages/ResetPasswordPage';
 import SignUpPage from './pages/SignUpPage';
@@ -11,14 +11,31 @@ import MarketMakerDashboardPage from './pages/MarketMakerDashboardPage';
 import ClientDashboardPage from './pages/ClientDashboardPage';
 import { auth } from './firebase'; // Import Firebase auth instance
 
-const ProtectedRoute = ({ element: Element, ...rest }) => {
-  const user = auth.currentUser; // Get current user
+const PrivateRoutes = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    // Clean up function to unsubscribe when component unmounts
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Show loading indicator while checking authentication state
+  }
 
   // If user is not logged in, redirect to login page
   if (!user) {
-    return <Navigate to="/login" />;
+    return <Navigate to="login" />;
   }
-  return <Element {...rest} />;
+
+  // If user is logged in, render the protected routes
+  return <Outlet />;
 };
 
 ReactDOM.render(
@@ -29,8 +46,10 @@ ReactDOM.render(
       <Route path="reset-password" element={<ResetPasswordPage />} />
       <Route path="sign-up" element={<SignUpPage />} />
       <Route path="submit-details" element={<SubmitDetailsPage />} />
-      <Route path="mm-dashboard" element={<ProtectedRoute element={MarketMakerDashboardPage} />} />
-      <Route path="client" element={<ProtectedRoute element={ClientDashboardPage} />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path='mm-dashboard' element={<MarketMakerDashboardPage />} />
+        <Route path='client' element={<ClientDashboardPage />} />
+      </Route>
     </Routes>
   </Router>,
   document.getElementById('root')
